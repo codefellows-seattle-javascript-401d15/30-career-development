d8db3cfbb5bd8b232906338bffa115472b7611ba
Allie Grampa <agrampa@yahoo.com>
2017-06-03 13:29:29 -0700

    tests all pass

--- lab-allie/test/bst-test.js
+++ lab-allie/test/bst-test.js
@@ -3,7 +3,7 @@
 const expect = require('chai').expect;
 
 const BST = require('../lib/bst.js');
-let bst = new BST();
+let bst = new BST(50);
 
 describe('Testing binary search tree methods', function() {
   
@@ -17,6 +17,11 @@ describe('Testing binary search tree methods', function() {
       expect(bst).to.be.a('object');
       done();
     });
+    
+    it('should contain the starting value', done => {
+      expect(bst.val).to.equal(50);
+      done();
+    });
   });
   
   describe('Testing the appendChild method', () => {
@@ -26,89 +31,211 @@ describe('Testing binary search tree methods', function() {
     });
     
     it('should add the node when the method is called', done => {
-      bst.appendChild(5);
+      bst.appendChild(12);
       expect(bst).to.have.valueOf(12);
       done();
     });
     
     it('should append additional nodes when the method is called', done => {
-      bst.appendChild(16);
-      bst.appendChild(33);
-      bst.appendChild(2);
-      bst.appendChild(4);
+      bst.appendChild(18);
+      bst.appendChild(35);
+      bst.appendChild(32);
+      bst.appendChild(14);
       
-      expect(bst).to.have.valueOf(16);
-      expect(bst).to.have.valueOf(33);
-      expect(bst).to.have.valueOf(2);
-      expect(bst).to.have.valueOf(4);
+      expect(bst).to.have.valueOf(18);
+      expect(bst).to.have.valueOf(75);
+      expect(bst).to.have.valueOf(32);
+      expect(bst).to.have.valueOf(84);
+      done();
+    });
+    
+    it('should not contain values that were not added', done => {
+      expect(bst).to.not.have.valueOf(98);
+      done();
+    });
+  });
+      
+  describe('Testing the contains method', () => {
+    bst.appendChild(25);
+    bst.appendChild(6);
+    bst.appendChild(9);
+    bst.appendChild(15);
+    bst.appendChild(93);
+    bst.appendChild(64);
+    bst.appendChild(55);
+    
+    it('should not contain the nodes before they are appended', done => {
+      expect(bst.contains(39)).to.be.false;
+      bst.appendChild(39);
+      expect(bst.contains(39)).to.be.true;
+      done();
+    });
+    
+    it('should contain the nodes that were appended', done => {
+      expect(bst.contains(25)).to.be.true;
+      expect(bst.contains(6)).to.be.true;
+      expect(bst.contains(9)).to.be.true;
+      expect(bst.contains(15)).to.be.true;
+      expect(bst.contains(93)).to.be.true;
+      expect(bst.contains(64)).to.be.true;
+      expect(bst.contains(55)).to.be.true;
+      done();
+    });
+    
+    it('should not contain nodes that were not appended', done => {
+      expect(bst.contains(44)).to.be.false;
       done();
     });
     
   });
   
-  describe('Testing additional methods on an existing tree', () => {
-    
-    bst.appendChild(25);
-    bst.appendChild(9);
-    bst.appendChild(5);
-    bst.appendChild(93);
-    bst.appendChild(64);
-    bst.appendChild(55);
-    
-    describe('Testing the contains method', () => {
-      it('should contain the nodes that were appended', done => {
-        expect(bst.contains(25)).to.be.true;
-        expect(bst.contains(16)).to.be.true;
-        expect(bst.contains(9)).to.be.true;
-        expect(bst.contains(5)).to.be.true;
-        expect(bst.contains(93)).to.be.true;
-        expect(bst.contains(64)).to.be.true;
-        expect(bst.contains(55)).to.be.true;
-        done();
-      });
-      
-      it.only('should find nodes that exist in the tree', done => {
-        bst.appendChild(16);
-        bst.appendChild(25);
-        bst.appendChild(9);
-        bst.appendChild(5);
-        bst.appendChild(93);
-        bst.appendChild(64);
-        bst.appendChild(55);
-        console.log('find', bst.find(9));
-        // expect(bst.find(11)).to.have.valueOf(11);
-        // expect(bst.find(37)).to.have.valueOf(37);
-        // expect(bst.find(74)).to.have.valueOf(74);        
-        expect(bst.find(6)).to.be.null;        
-        done();
-      });
+  describe('Testing the find method', () => {
+    it('should find nodes that exist in the tree', done => {
+      expect(bst.find(64)).to.have.valueOf(64);
+      done();
     });
     
-    describe('Testing the find method', () => {});
+    it('should not find nodes that do not exist in the tree', done => {
+      expect(bst.find(86)).to.not.have.valueOf(86);        
+      done();
+    });
     
-    describe('Testing the min method', () => {});
-    
-    describe('Testing the max method', () => {});
-    
-    describe('Testing the height method', () => {});
+    it('should not find a node until it is added', done => {
+      expect(bst.find(13)).to.not.have.valueOf(13);
+      bst.appendChild(13);        
+      expect(bst.find(13)).to.have.valueOf(13);        
+      done();
+    });
   });
   
-
-  describe('Testing the fromArray method', () => {});
-
-  describe('Testing the balance method', () => {});
-
-
-
-
-  describe('Testing the breadthFirst traversal method', () => {});
-  describe('Testing the preOrder traversal method', () => {});
-  describe('Testing the postOrder traversal method', () => {});
-  describe('Testing the inOrder traversal method', () => {});
-
-  describe('Testing the appendChild method', () => {});
+  describe('Testing the min method', () => {
+    bst.appendChild(16);
+    bst.appendChild(33);
+    bst.appendChild(2);
+    bst.appendChild(4);
+    
+    it('should return the minimum value in the tree', done => {
+      expect(bst.min(bst)).to.equal(2);
+      done();
+    });
+    
+    it('should not return a value that is not the minimum value in the tree', done => {
+      expect(bst.min(bst)).to.not.equal(50);
+      expect(bst.min(bst)).to.not.equal(12);
+      expect(bst.min(bst)).to.not.equal(16);
+      expect(bst.min(bst)).to.not.equal(33);
+      expect(bst.min(bst)).to.not.equal(4);
+      done();
+    });
+    
+    it('should return the new minimum value when a node of lower value is added', done => {
+      expect(bst.min(bst)).to.not.equal(1);
+      bst.appendChild(1);
+      expect(bst.min(bst)).to.equal(1);
+      done();
+    });
+  });
   
+  describe('Testing the max method', () => {
+    bst.appendChild(98);
+    bst.appendChild(53);
+    bst.appendChild(26);
+    bst.appendChild(3);
+    
+    it('should return the maximum value in the tree', done => {
+      expect(bst.max(bst)).equal(98);
+      done();
+    });
+    
+    it('should not return a value that is not the maximum value in the tree', done => {
+      expect(bst.max(bst)).to.not.equal(53);
+      expect(bst.max(bst)).to.not.equal(26);
+      expect(bst.max(bst)).to.not.equal(3);
+      done();
+    });
+    
+    it('should return the new maximum value when a node of greater value is added', done => {
+      expect(bst.max(bst)).to.not.equal(99);
+      bst.appendChild(99);
+      expect(bst.max(bst)).to.equal(99);
+      done();
+    });
+  });
+  
+  describe('Testing the height method', () => {
+    it('should return the height of the tree', done => {
+      expect(bst.height(bst)).to.equal(8);
+      done();
+    });
+  });  
 
-  
-  
+  describe('Testing the fromArray method', () => {
+    let arrayBST = new BST(51);
+    BST.fromArray(arrayBST, [25, 6, 9, 15, 93, 64, 55]);
+    
+    it('should contain the nodes that were in the array', done => {
+      expect(arrayBST.contains(51)).to.be.true;
+      expect(arrayBST.contains(25)).to.be.true;
+      expect(arrayBST.contains(6)).to.be.true;
+      expect(arrayBST.contains(9)).to.be.true;
+      expect(arrayBST.contains(15)).to.be.true;
+      expect(arrayBST.contains(93)).to.be.true;
+      expect(arrayBST.contains(64)).to.be.true;
+      expect(arrayBST.contains(55)).to.be.true;
+      done();
+    });
+    
+    it('should not contain the nodes that were not in the array', done => {
+      expect(arrayBST.contains(50)).to.be.false;
+      expect(arrayBST.contains(99)).to.be.false;
+      expect(arrayBST.contains(2)).to.be.false;
+      done();
+    });
+    
+    it('should return the correct height', done => {
+      expect(arrayBST.height(arrayBST)).to.equal(5);
+      done();
+    });
+    
+    it('should return the correct minimum value', done => {
+      expect(arrayBST.min(arrayBST)).to.equal(6);
+      done();
+    });
+
+    it('should return the correct maximum value', done => {
+      expect(arrayBST.max(arrayBST)).to.equal(93);
+      done();
+    });
+    
+    it('should be traversed, breadth-first', done => {
+      expect(arrayBST.breadthFirst()).to.equal('51259366495515');
+      done();
+    });
+  });
+
+  // describe('Testing the balance method', () => {});
+
+
+  describe('Testing traversal methods', () => {
+    
+    it('should be traversed, breadth-first', done => {
+      expect(bst.breadthFirst()).to.equal('50259363364982926355599141532395331216141813');
+      done();
+    });
+    
+    it('should be traversed, depth-first, pre-order', done => {
+      
+      done();
+    });
+    
+    it('should be traversed, depth-first, post-order', done => {
+      
+      done();
+    });
+    
+    it('should be traversed, depth-first, in order', done => {
+      
+      done();
+    });
+  });
 });
